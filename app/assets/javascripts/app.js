910b2f9a21b348309d50800065972458b6f6bac8
Dan Sokol <dafaso1@gmail.com>
2017-01-06 09:48:00 -0500

    added donors and submit button

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,8 +7,8 @@ $("html").click(function(){
    $("#drop-menu").slideUp(20,function(){
        open=false;
    });
-        
-    };    
+
+    };
 });
 
 $(".menu-about").click(function(e) {
@@ -21,7 +21,7 @@ $(".menu-about").click(function(e) {
             open=false;
         };
     });
-});    
+});
 
 
 //$(".r").click(function(e){
@@ -46,41 +46,44 @@ $(".donate-button").click(function(){
 
 $(window).resize(function(){
     if($(this).width() > 1020){
-   $("#mobile_menu").slideUp(100); 
-   $(".hamburger").removeClass("hamburger_toggle");   $("#mobile-drop").slideUp(100);     
+   $("#mobile_menu").slideUp(100);
+   $(".hamburger").removeClass("hamburger_toggle");   $("#mobile-drop").slideUp(100);
     }//close full screen drop menu when pc screen increases.
 });
 
 
+//
+// $('.gallery').justifiedGallery({
+//     rowHeight : 150,
+//     lastRow : 'nojustify',
+//     margins : 5,
+//     border : 5,
+//
+//
+//     sizeRangeSuffixes : {
+//         'lt100':'_a',
+//         'lt240':'_b',
+//         'lt320':'_c',
+//         'lt500':'_d',
+//         'lt640':'_e',
+//         'lt1024':'_f'
+//     },
+//
+//     captionSettings : {
+//         animationDuration: 1100,
+//         visibleOpacity: 0.9,
+//         nonVisibleOpacity: 0.0
+//     },
+//
+//     rel : 'gallery1',
+//
+//
+// });
 
-$('.gallery').justifiedGallery({
-    rowHeight : 150,
-    lastRow : 'nojustify',
-    margins : 5,
-    border : 5, 
-    
-    
-    sizeRangeSuffixes : {
-        'lt100':'_a', 
-        'lt240':'_b', 
-        'lt320':'_c', 
-        'lt500':'_d', 
-        'lt640':'_e', 
-        'lt1024':'_f'
-    },
-    
-    captionSettings : {
-        animationDuration: 1100,
-        visibleOpacity: 0.9,
-        nonVisibleOpacity: 0.0
-    },
-    
-    rel : 'gallery1',
-    
-    
-});
+
+
 //    .on('jg.complete', function () {
 //    $(this).find('a').baguetteBox({
-//       
+//
 //    });
 //});
