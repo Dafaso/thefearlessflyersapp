64fec3b4c4e803140d7b30c445bab1cc6e884feb
dafaso <dafaso1@gmail.com>
2017-02-21 14:34:17 -0500

    footer logos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+   
+    def logos
+
+        html = "\t<li>" + link_to(image_tag('outcyclinglogo.png'), 'http://www.outcycling.org/', target:"_blank") + "</li>\n"
+        
+        html += "\t<li>" + link_to(image_tag('center_logo.png'), 'https://gaycenter.org/' , target:"_blank")  + "</li>\n"
+        
+        
+#        html += "<li>" + link_to(image_tag('bike_ny.png'), 'http://www.bike.nyc/', target:"_blank") + "\n"
+        
+      
+        html.html_safe 
+        
+    end 
+    
 end
